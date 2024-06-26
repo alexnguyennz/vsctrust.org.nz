11d83bd4f6d1293a38afd36b633bc94667c6aef6
Alex <dev@alexnguyen.co.nz>
2024-06-26 15:45:12 +1200

    styling and updated logo slider

--- src/components/pages/home/Quotes.tsx
+++ src/components/pages/home/Quotes.tsx
@@ -1,39 +1,29 @@
-import { cn } from "@/lib/utils";
 import Marquee from "@/components/ui/marquee";
 
 const quotes = [
   {
-    name: "Anonymous",
     quote: "Thank you for all that you do to take care of people in the city.",
   },
   {
-    name: "Anonymous",
     quote: "Love Take 10, thanks for all the positive mahi.",
   },
   {
-    name: "Anonymous",
     quote:
       "These guys offer such a valuable service, having an opportunity to stop and rest is a very important mission.",
   },
   {
-    name: "Anonymous",
     quote:
       "I feel like they truly care about people on the street, making sure they are getting home safely.",
   },
 ];
 
-const Card = ({ name, quote }: { name: string; quote: string }) => {
+const Card = ({ name, quote }: { name?: string; quote: string }) => {
   return (
     <figure
       className={
         "relative w-72 cursor-default overflow-hidden rounded-2xl border border-gray-200 bg-gray-50 p-5 transition-colors hover:bg-gray-100"
       }
     >
-      <div className="flex flex-row items-center gap-2">
-        <div className="flex flex-col">
-          <figcaption className="font-bold">{name}</figcaption>
-        </div>
-      </div>
       <blockquote>{quote}</blockquote>
     </figure>
   );
