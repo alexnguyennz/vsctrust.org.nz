807c6d2f900cb657ac45a1dbf654bc00bf31d6fa
Alex <dev@alexnguyen.co.nz>
2024-06-28 14:58:36 +1200

    gallery feature and general styling

--- src/components/header/MobileNavigation.tsx
+++ src/components/header/MobileNavigation.tsx
@@ -1,50 +1,34 @@
-"use client";
-
-import { aboutUsLinks, ourWorkLinks, getInvolvedLinks } from "@/lib/links.tsx";
+import { menuLinks } from "@/lib/links.tsx";
 import { MobileSubmenu } from "@/components/header/MobileSubmenu";
 
 export function MobileNavigation() {
   return (
     <ul className={"mt-4 space-y-4 font-extrabold"}>
       <li tabIndex={0}>
-        <a href="/" className={"block p-1"} rel="prefetch-intent" tabIndex={1}>
+        <a href="/" className={"block p-1"}>
           Home
         </a>
       </li>
-      <MobileSubmenu name={"About Us"}>
-        {aboutUsLinks.map((link) => (
-          <li key={link.title} title={link.title}>
-            <a href={link.href} className="block p-1" rel="prefetch">
-              {link.title}
-            </a>
-          </li>
-        ))}
-      </MobileSubmenu>
-      <MobileSubmenu name={"Our Work"}>
-        {ourWorkLinks.map((link) => (
-          <li key={link.title} title={link.title}>
-            <a href={link.href} className="block p-1" rel="prefetch">
-              {link.title}
-            </a>
-          </li>
-        ))}
-      </MobileSubmenu>
-      <MobileSubmenu name={"Get Involved"}>
-        {getInvolvedLinks.map((link) => (
-          <li key={link.title} title={link.title}>
-            <a href={link.href} className="block p-1" rel="prefetch">
-              {link.title}
-            </a>
-          </li>
-        ))}
-      </MobileSubmenu>
+
+      {Object.keys(menuLinks).map((menu) => (
+        <MobileSubmenu name={menu}>
+          {menuLinks[menu].map((link) => (
+            <li key={link.title} title={link.title}>
+              <a href={link.href} className="block p-1" rel="prefetch">
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </MobileSubmenu>
+      ))}
+
       <li>
-        <a href="/contact/" className={"block p-1"} rel="prefetch">
+        <a href="/contact/" className={"block p-1"}>
           Contact
         </a>
       </li>
       <li>
-        <a href="/donate/" className={"block p-1"} rel="prefetch">
+        <a href="/donate/" className={"block p-1"}>
           Donate
         </a>
       </li>
--- src/lib/links.tsx
+++ src/lib/links.tsx
@@ -6,7 +6,6 @@ import {
   Newspaper,
   type LucideIcon,
 } from "lucide-react";
-
 import {
   IconBackpack,
   IconFriends,
@@ -84,7 +83,7 @@ export const menuLinks: MenuLinks = {
     {
       title: "Generation Link",
       href: "/gen-link/",
-      description: "Connect kids with elders",
+      description: "Connecting kids with elders",
       icon: GenLink,
     },
   ],
@@ -98,7 +97,7 @@ export const menuLinks: MenuLinks = {
     {
       title: "Volunteer",
       href: "/volunteer/",
-      description: "How you can help out",
+      description: "How you can help",
       icon: HeartHandshake,
     },
     {
@@ -109,84 +108,3 @@ export const menuLinks: MenuLinks = {
     },
   ],
 };
-
-export const aboutUsLinks: {
-  title: string;
-  href: string;
-  description: string;
-}[] = [
-  {
-    title: "Who We Are",
-    href: "/who-we-are/",
-    description: "More about us",
-  },
-  {
-    title: "The Team",
-    href: "/team/",
-    description: "Our team",
-  },
-  {
-    title: "The Trust",
-    href: "/trust/",
-    description: "Our trustees",
-  },
-  {
-    title: "News",
-    href: "/news/",
-    description: "Stay up-to-date",
-  },
-  {
-    title: "Media",
-    href: "/media/",
-    description: "Our media coverage",
-  },
-];
-
-export const ourWorkLinks: {
-  title: string;
-  href: string;
-  description: string;
-}[] = [
-  {
-    title: "Do Some Good",
-    href: "/dosomegood/",
-    description: "Helping tamariki in need",
-  },
-  {
-    title: "Take 10",
-    href: "/take10/",
-    description: "Late-night safe zone",
-  },
-  {
-    title: "Take 10 Arvo's",
-    href: "/take10-arvos/",
-    description: "After-school youth zone",
-  },
-  {
-    title: "Generation Link",
-    href: "/gen-link/",
-    description: "Connect kids with elders",
-  },
-];
-
-export const getInvolvedLinks: {
-  title: string;
-  href: string;
-  description: string;
-}[] = [
-  {
-    title: "Donate",
-    href: "/donate/",
-    description: "Support our work",
-  },
-  {
-    title: "Volunteer",
-    href: "/volunteer/",
-    description: "How you can help out",
-  },
-  {
-    title: "Sponsor a School",
-    href: "/sponsor-a-school/",
-    description: "Partner with a school",
-  },
-];
