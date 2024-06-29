e0e17c266b70bd2ec8f6c233df3d1b50bb58f09c
Alex <dev@alexnguyen.co.nz>
2024-06-29 15:35:51 +1200

    style programme cards

--- src/components/ProgrammeLogos.tsx
+++ src/components/ProgrammeLogos.tsx
@@ -1,15 +1,35 @@
 export function DoSomeGood() {
-  return <img src={"/logo/dosomegood.png"} alt={""} className={"size-6"} />;
+  return (
+    <img
+      src={"/logo/dosomegood.png"}
+      alt={"Do Some Good logo"}
+      className={"size-6"}
+    />
+  );
 }
 
 export function Take10() {
-  return <img src={"/logo/take10.png"} alt={""} className={"size-6"} />;
+  return (
+    <img src={"/logo/take10.png"} alt={"Take 10 logo"} className={"size-6"} />
+  );
 }
 
 export function Take10Arvos() {
-  return <img src={"/logo/take10-arvos.png"} alt={""} className={"size-6"} />;
+  return (
+    <img
+      src={"/logo/take10-arvos.png"}
+      alt={"Take 10 Arvo's logo"}
+      className={"size-6"}
+    />
+  );
 }
 
 export function GenLink() {
-  return <img src={"/logo/gen-link.png"} alt={""} className={"size-6"} />;
+  return (
+    <img
+      src={"/logo/gen-link.png"}
+      alt={"Gen Link logo"}
+      className={"size-6"}
+    />
+  );
 }
