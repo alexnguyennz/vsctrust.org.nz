b01cfe539f68c79ac862b436f04025718ae5e6cf
Alex <dev@alexnguyen.co.nz>
2024-07-06 10:16:59 +1200

    gallery image optimization

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,6 @@ module.exports = {
     extend: {
       animation: {
         marquee: "marquee var(--duration) linear infinite",
-        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "collapsible-down": "collapsible-down 0.2s ease-out",
@@ -19,10 +18,6 @@ module.exports = {
         marquee: {
           from: { transform: "translateX(0)" },
           to: { transform: "translateX(calc(-100% - var(--gap)))" },
-        },
-        "marquee-vertical": {
-          from: { transform: "translateY(0)" },
-          to: { transform: "translateY(calc(-100% - var(--gap)))" },
         },
         "accordion-down": {
           from: { height: "0" },
