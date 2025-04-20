d1abb3cddc9d19ab21bdeb5680a3aa3292f8fa77
Alex <dev@alexnguyen.co.nz>
2025-04-21 11:45:31 +1200

    Remove tabIndex from mobile navigation

--- src/components/header/MobileNavigation.tsx
+++ src/components/header/MobileNavigation.tsx
@@ -9,31 +9,33 @@ import {
 
 export function MobileNavigation() {
   return (
-    <ul tabIndex={-1} className={"mt-6 font-extrabold"}>
-      <li tabIndex={0} className={"py-2"}>
-        <a href="/" className={"block"}>
+    <ul className={"mt-6 font-extrabold"}>
+      <li>
+        <a href="/" className="block py-2">
           Home
         </a>
       </li>
 
-      <Accordion type="single" collapsible>
-        {Object.keys(menuLinks).map((menu) => (
-          <AccordionItem value={menu} key={menu}>
-            <AccordionTrigger>{menu}</AccordionTrigger>
-            <AccordionContent>
-              <ul className="space-y-3 py-2">
-                {menuLinks[menu].map((link) => (
-                  <li key={link.title} title={link.title}>
-                    <a href={link.href} className="block p-1">
-                      {link.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
+      <li>
+        <Accordion type="single" collapsible>
+          {Object.keys(menuLinks).map((menu) => (
+            <AccordionItem value={menu} key={menu}>
+              <AccordionTrigger>{menu}</AccordionTrigger>
+              <AccordionContent>
+                <ul className="space-y-3 py-2">
+                  {menuLinks[menu].map((link) => (
+                    <li key={link.title} title={link.title}>
+                      <a href={link.href} className="block p-1">
+                        {link.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </li>
 
       <li>
         <a href="/contact/" className={"block py-2"}>
